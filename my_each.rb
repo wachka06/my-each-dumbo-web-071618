bbdbbd32b4c1f7add3448a72136417be9726018f
wachka06 <wachka06@gmail.com>
2018-07-10 04:15:43 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
 def my_each(array) # put argument(s) here
   # code here
-  i = 0
+  i = 0 
+  
+  while i < array.length
+    yield(array[i])
+    i += 1 
+  end 
+  array 
  
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
- 
-    array
 end
